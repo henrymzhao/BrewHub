8ac1ed4d1087c8ccfba2aea6f307b68a3d12cf53
henrymzhao <henrymzhao@gmail.com>
2016-06-11 21:13:22 -0700

    EMBARKING NEW ADVENTURE: HTTP PAGES

--- BrewHub/config/routes.rb
+++ BrewHub/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   get 'users/new'
 
   get 'sessions/new'
@@ -7,7 +11,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'users#new'
+   resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
