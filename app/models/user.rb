4203db3a8dd971090538f202ebcfcadea403d256
Edmond <edmond.e321@gmail.com>
2016-06-13 07:39:51 -0700

    added bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
 	validates :name, presence: true, uniqueness: true,
 	format: {with: /\A[a-zA-Z]+\z/, message: "only allows letters"}
 	validates :password, presence: true,
 	length: {in: 6..20}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+    has_secure_password
 end
 
 user = User.new
+
 user.valid? # => false
 user.errors[:name]
 # => ["can't be blank", "name already exists"]
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160611224913) do
+ActiveRecord::Schema.define(version: 20160613142936) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "email"
+    t.string   "password_digest"
   end
 
 end
