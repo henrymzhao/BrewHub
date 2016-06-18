685e035265d6b03d3c933ddb7ed5270c2c7391d2
eeng321 <e_eng321@hotmail.com>
2016-06-17 17:19:14 -0700

    Add files via upload
    login error message added

--- BrewHub/app/controllers/users_controller.rb
+++ BrewHub/app/controllers/users_controller.rb
@@ -12,39 +12,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # def create
-  #   @user = User.new(user_params)
-  #    respond_to do |format|
-  #     if @user.save
-        
-  #       #format.json { render :show, status: :created, location: @user }
-  #       session[:user_id] = @user.id
-  #       #redirect_to '/'
-  #       # format.html { redirect_to 'index' }
-  #       format.html { redirect_to '/users' }
-  #     else
-  #       format.html { render '/signup' }
-  #       #format.json { render json: @user.errors, status: :unprocessable_entity }
-  #       #redirect_to '/signup'
-  #       # format.html { redirect_to '/signup' }
-  #       # format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-def create
-  @user = User.new(user_params)
-
-  respond_to do |format|
-    if @user.save
-      # flash[:notice] = 'User was successfully created.'
-      format.html { redirect_to "index"}
-      # format.xml { render xml: @user, status: :created, location: @user }
-    else
-      format.html.none { render "new" }
-      # format.xml { render xml: @user.errors, status: :unprocessable_entity }
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        #format.html { redirect_to @user }
+        #format.json { render :show, status: :created, location: @user }
+        session[:user_id] = @user.id
+        #redirect_to '/'
+        format.html { redirect_to '/' }
+      else
+        #format.html { render :new }
+        #format.json { render json: @user.errors, status: :unprocessable_entity }
+        #redirect_to '/signup'
+        #format.html { redirect_to '/signup' }
+        format.html.none { render "new" }
+      end
     end
   end
-end
 
   private
     def user_params
