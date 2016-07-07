0717713370099a82d68370fef76340f5da62069b
Jonathan Loewen <jmloewen@sfu.ca>
2016-07-06 19:19:30 -0700

    figured out beers from breweris

--- app/controllers/browser_controller.rb
+++ app/controllers/browser_controller.rb
@@ -1,16 +1,24 @@
 class BrowserController < ApplicationController
 
+  def new
+    @browser = Browser.new
+    @beer = Beer.new
+  end
+
   def beers
     brewery_db = BreweryDB::Client.new do |config|
       config.api_key = API_KEY
     end
-    @beers = brewery_db.beers.all(abv: '5.5')
+    @beers = brewery_db.beers.all(withBreweries: 'Y')
   end
 
   def pubs
     brewery_db = BreweryDB::Client.new do |config|
       config.api_key = API_KEY
     end
-    @pubs = brewery_db.breweries.all(name: "a*")
+    
+    
+    
+    @pubs = brewery_db.brewery('AAj4GG').beers()
   end
 end
