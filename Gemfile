52e9cdc2f6a274d2618e1f640813fd4460d3119b
JanMoat <cracroft@gmail.com>
2016-07-11 01:15:32 -0700

    Add files via upload

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'coffee-rails', '~> 4.1.0'
 
 # facebook omniauth
 gem 'omniauth-facebook'
+# google omniauth
+gem "omniauth-google-oauth2", "~> 0.2.1"
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '669387803208484', '5f32d51f274ba47c88eb7cdb07b03781'
+  provider :google_oauth2, '646725351046-2hhcmosk3vsdl97lq7csfgk6gbtpl2bu.apps.googleusercontent.com', 'wsxaPh_nMJdSStzanrD40Wlq', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
