915dfaf5914ea604b1e72dc26aaa2436d4004c72
Janice Moat <cracroft@gmail.com>
2016-07-28 15:35:14 -0700

    derp

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ActiveRecord::Base
     read_attribute('avatar') || DEFAULT_AVATAR
   end
   # standard validation
-  validates :avatar, :url => {:allow_nil => true}, :url => {:allow_blank => true}, :url => {:no_local => true}
+  validates :avatar, :url => {:allow_blank => true}
 
   #validates :avatar, :presence => true, :avatar => true
 
