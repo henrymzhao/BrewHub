c0aadb35f8939ef63025211d8890de6be9bde718
Janice Moat <cracroft@gmail.com>
2016-07-29 16:51:38 -0700

    derp

--- app/validators/image_validator.rb
+++ app/validators/image_validator.rb
@@ -1,12 +1,19 @@
 class ImageValidator < ActiveModel::EachValidator
+
+  unless value.chars.first == "h"
+    record.errors[attribute] << (options[:message] || "is not a valid url")
+  end
+
   def validate_each(record, attribute, value)
     return if value.blank?
     begin
       uri = URI.parse(value)
       resp = FastImage.type(uri)
-   rescue URI::InvalidURIError
-      resp = false
-    end
+
+#   rescue URI::InvalidURIError
+#      resp = false
+#    end
+
     unless resp == :gif || resp == :jpeg || resp == :png
       record.errors[attribute] << (options[:message] || "is not a valid url")
     end
