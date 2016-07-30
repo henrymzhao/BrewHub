4f24e4b84c26d3a8bd008c3202ed8798a4a81a2e
Janice Moat <cracroft@gmail.com>
2016-07-29 21:45:50 -0700

    derp

--- app/validators/image_validator.rb
+++ app/validators/image_validator.rb
@@ -1,33 +1,6 @@
 class ImageValidator < ActiveModel::EachValidator
 
 
-  def validates_format_of_url(*attr_names)
-    require 'uri/http'
-
-    configuration = { :on => :save, :schemes => %w(http https) }
-    configuration.update(attr_names.extract_options!)
-
-    allowed_schemes = [*configuration[:schemes]].map(&:to_s)
-
-    validates_each(attr_names, configuration) do |record, attr_name, value|
-      begin
-        uri = URI.parse(value)
-
-        if !allowed_schemes.include?(uri.scheme)
-          raise(URI::InvalidURIError)
-        end
-
-        if [:scheme, :host].any? { |i| uri.send(i).blank? }
-          raise(URI::InvalidURIError)
-        end
-
-      rescue URI::InvalidURIError => e
-        record.errors.add(attr_name, :invalid, :default => configuration[:message], :value => value)
-        next
-      end
-    end
-  end
-
 
 
 
