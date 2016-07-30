d86319f9b3146ce39c6e7a73541650f584e6bdd7
Janice Moat <cracroft@gmail.com>
2016-07-29 18:15:56 -0700

    derp

--- app/validators/image_validator.rb
+++ app/validators/image_validator.rb
@@ -1,6 +1,17 @@
 class ImageValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
+    valid = begin
+      URI.parse(value).kind_of?(URI::HTTP)
+    rescue URI::InvalidURIError
+      false
+    end
+    unless valid
+      record.errors[attribute] << (options[:message] || "is an invalid URL")
+    end
+  end
+
+  def validate_each(record, attribute, value)
     return if value.blank?
     begin
       uri = URI.parse(value)
