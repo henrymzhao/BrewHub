1e5b552e886fc2951f21c725b18afed8d000d6c8
Janice Moat <cracroft@gmail.com>
2016-07-29 18:08:08 -0700

    derp

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,8 +30,8 @@ class User < ActiveRecord::Base
     read_attribute('avatar') || DEFAULT_AVATAR
   end
 
-  validates :avatar, :url => true, :image => true
-
+  #validates :avatar, :url => true, :image => true
+  validates :avatar, :image => true, :if => :url
 
 
   def self.create_with_omniauth(auth)
