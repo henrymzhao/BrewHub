f789c05aa74885cfe1f42ab1f444eb78676944f9
Jonathan Loewen <jmloewen@sfu.ca>
2016-07-31 21:17:54 -0700

    testing location-based brewery selection

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,5 +2,5 @@ class Brewery < ActiveRecord::Base
   has_many :beers
   validates_uniqueness_of :brewery_id
   geocoded_by :ip_address
-  #after_validation :geocode
+  after_validation :geocode
 end
